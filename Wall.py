6c7119fafb09c3f3cfea7f6497ecb3b18563a6d9
conor-coding <65498907+conor-coding@users.noreply.github.com>
2020-10-12 12:25:35 +0100

    Create Wall.py

---
+++ Wall.py
@@ -0,0 +1,34 @@
+#Brick code
+
+bricklenght = 75
+brickwidth = 225
+brickarea = bricklenght * brickwidth
+brick = brickarea
+#print(brick)
+
+#Block flat code
+blockflatlenght = 112.5
+blockflatwidth = 450
+blockflatarea = blockflatlenght * blockflatwidth
+blockflat = blockflatarea
+
+#Block edge code
+blockedgelenght = 225
+blockedgewidth = 450
+blockedgearea =blockedgelenght * blockedgewidth
+blockedge = blockedgearea
+
+
+#Wall code
+x = 2700
+y = 1800
+
+
+userarea = x * y
+wall = userarea / blockflat
+print(wall) 
+
+
+
+
+
