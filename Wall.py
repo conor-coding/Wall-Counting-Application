ce8ad92e7828e21c84ebefb10c994543ec4d4630
conor-coding <65498907+conor-coding@users.noreply.github.com>
2020-10-17 17:50:56 +0100

    Update Wall.py

--- Wall.py
+++ Wall.py
@@ -1,19 +1,25 @@
 #Code in form of a class
 
-class masonary:
+class Masonary:
 
-    def __init__(self, length, width):
-        self.length = length
+    def __init__(self, height, width):
+        self.height = height
         self.width = width
 
     def area(self):
-        return self.length*self.width
+        return self.height*self.width
 
-brick = masonary(75, 225)
-blockflat = masonary(112.5, 450)
-blockedge = masonary(225, 450)
+def amount_count(self, wall):
+    return brick.area()/ wall.area()
+    
+
+brick = Masonary(75, 225)
+blockflat = Masonary(112.5, 450)
+blockedge = Masonary(225, 450)
 
 
-wall = masonary(2700, 1800)
+wall = Masonary(2700, 1800)
+    
 
 print(wall.area())
+print(wall.area()/brick.area())
