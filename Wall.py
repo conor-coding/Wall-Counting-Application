6236d44496af423c4fab71152ae8c2c92fd33ddb
conor-coding <65498907+conor-coding@users.noreply.github.com>
2020-10-26 08:38:19 +0000

    Update Wall.py

--- Wall.py
+++ Wall.py
@@ -16,12 +16,15 @@ blockedge = Masonry(225, 450)
 class Wall(Masonry):
     def __init__(self, height, width):
         super().__init__(height, width)
+    #Attempting to divide the sample wall area by the brick area
     def __truediv__(self, other):
         return self.area/ other.area
 
+#sample wall, I named it sample as not to confuse it with the Wall class
 sample = Wall(2700, 1800)
     
-
-print(sample.area())
+#This print statement is my bane. I can't work out the formula to divide two methods together.
 print(Wall.__truediv__(sample, brick))
-#print(type(brick.area))
+#These print statements are used to test if things are working.
+#print(sample.area())
+#print(brick.area())
