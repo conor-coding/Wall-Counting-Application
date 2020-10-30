ade9c059547476b33ac6f23bbb1c6e2d7673ba7c
conor-coding <65498907+conor-coding@users.noreply.github.com>
2020-10-30 19:59:08 +0000

    Create roses.py

---
+++ roses.py
@@ -0,0 +1,12 @@
+#This is some input testing before I attempt to use it in Wall.py
+
+#test this file
+
+colour = input("Enter a colour: ")
+noun = input("enter a noun: ")
+celebraty = input("Enter a Celebraty: ")
+
+
+print("Roses are " + colour)
+print(noun + " are Blue")
+print("I Love " + celebraty)
